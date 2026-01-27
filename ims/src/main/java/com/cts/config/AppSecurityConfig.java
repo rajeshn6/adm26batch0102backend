16c108037e76f3d02e74739b6dd13a2c80df69d6
N <2393375@cognizant.com>
2026-01-27 17:13:13 +0530

    jwt token added along with spring security

--- ims/src/main/java/com/cts/config/AppSecurityConfig.java
+++ ims/src/main/java/com/cts/config/AppSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.cts.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -17,26 +18,33 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfig {
 	//UserDetailsService userDetailsService=new InMemoryUserDetailsManager();
-
+	@Autowired
+	private JwtFilter jwtFilter;
+	
  	@Bean
  	SecurityFilterChain securityFilterChain(HttpSecurity http)  {
  		http.csrf(req->req.disable());
  		http.cors(req->req.disable());
  		http.authorizeHttpRequests(req->
  		req.requestMatchers("/api/user").permitAll()
- 		
+ 		.requestMatchers("/api/user/login").permitAll()
  		.anyRequest().authenticated()
  		);
  		http.httpBasic(Customizer.withDefaults());
- 			
+ 		http.addFilterBefore(jwtFilter, BasicAuthenticationFilter.class);
  		return http.build();
  	}
+	@Bean
+	AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
+	}
 // 	@Bean 
 // 	AuthenticationProvider authenticationProvider() {
 // 		 DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService());
--- ims/src/main/java/com/cts/controller/UserController.java
+++ ims/src/main/java/com/cts/controller/UserController.java
@@ -4,11 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cts.config.JwtUtility;
+import com.cts.model.LoginDto;
 import com.cts.model.UserDto;
 import com.cts.service.UserService;
 
@@ -18,11 +24,43 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Autowired
+	private JwtUtility jwtUtility;
+	
 	@PostMapping
 	public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
 		UserDto userDto1=userService.createUser(userDto);
 		return new ResponseEntity<UserDto>(userDto1,HttpStatus.CREATED);
 	}
+	@PostMapping("/login")
+	public String login(@RequestBody LoginDto loginDto) {
+//		loginDto.getUsername()loginDto --send it to 
+//		generate the token
+		System.out.println("login called");
+	   	 Authentication authentication = authenticationManager.authenticate(
+	   	            new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword()));
+	   	        if (authentication.isAuthenticated()) {
+	   	            return jwtUtility.generateToken(loginDto.getUsername());
+	   	        } else {
+	   	            throw new UsernameNotFoundException("Invalid user email and password request!");
+	   	        }
+			
+	}
 	
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
